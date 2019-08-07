ac8ccdefb662e5fea3cfbfb7bbf636af1cfe7635
Anthony Moss <asmoss93@gmail.com>
2019-08-07 18:35:19 -0400

    adds more info to console log of weather, and changes language to spanish

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const request = require('request');
 
-const  url = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233'
+const  url = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233?lang=es'
 
 request({ url: url, json: true }, (error, response) => {
     // const data = JSON.parse(response.body)
     // console.log(response.body.currently)
-    console.log(`It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} percent change of rain`)
+    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} % chance of rain`)
+
 })
