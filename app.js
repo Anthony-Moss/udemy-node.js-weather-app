7a38d80e2cc6172682fe6e8ea272936cd3fa5157
Anthony Moss <asmoss93@gmail.com>
2019-08-07 18:55:53 -0400

    brings in mapbox api for geolocation, console logs the lat/long of an address/place

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const request = require('request');
 
-const  url = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233'
+const  weatherUrl = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233'
+const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic3dpZnRtYXBsZSIsImEiOiJjanRjNjZyNGEwaGN1M3lxandlbzRnbGQ5In0.EWCNMHBKSS6VY6o5UFh6Ew&limit=1'
 
-request({ url: url, json: true }, (error, response) => {
-    // const data = JSON.parse(response.body)
-    // console.log(response.body.currently)
-    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} % chance of rain`)
+// request({ url: weatherUrl, json: true }, (error, response) => {
+//     // const data = JSON.parse(response.body)
+//     // console.log(response.body.currently)
+//     console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} % chance of rain`)
 
+// })
+
+// Geocoding
+// user provides address => get lat/long from geo api => put lat/long into weath api and get weather
+request({url: geoUrl, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(`The latitude is ${latitude} the longitude is ${longitude}`)
 })
