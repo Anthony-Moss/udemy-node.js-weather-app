77c31c7c18ecceab84cace0cf2ab60d86a6fb240
Anthony Moss <asmoss93@gmail.com>
2019-08-07 17:29:55 -0400

    adds alternate way of doing the http request

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const  url = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    // const data = JSON.parse(response.body)
+    console.log(response.body.currently)
 })
