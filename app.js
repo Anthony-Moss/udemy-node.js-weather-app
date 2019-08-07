b90b6e120d4055c1bf6fb8f5583dce57e6539dd1
Anthony Moss <asmoss93@gmail.com>
2019-08-07 17:24:11 -0400

    adds simple http request to the weather api test data

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const request = require('request');
+
+const  url = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233'
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
