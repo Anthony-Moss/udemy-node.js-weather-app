b370f51eaee91bf058a6c91b69caf33a03b26421
Anthony Moss <asmoss93@gmail.com>
2019-08-07 18:35:55 -0400

    changes language back to english

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const  url = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233?lang=es'
+const  url = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233'
 
 request({ url: url, json: true }, (error, response) => {
     // const data = JSON.parse(response.body)
