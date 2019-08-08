8f653be13d97b80b0a9468592ffcd8a56aabf92c
Anthony Moss <asmoss93@gmail.com>
2019-08-07 23:25:01 -0400

    adds function calls for geocode and forecast to app.js

--- app.js
+++ app.js
@@ -1,56 +1,23 @@
 const request = require('request');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const  weatherUrl = 'https://api.darksky.net/forecast/5ff7d44666836f47201faa8a389d0db4/37.8267,-122.4233'
-
-// request({ url: weatherUrl, json: true }, (error, response) => {
-//     // const data = JSON.parse(response.body)
-//     // console.log(response.body.currently)
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location input!')
-//     } else {
-//         console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} % chance of rain`)
-//     }
-// })
-
-// Geocoding
-// user provides address => get lat/long from geo api => put lat/long into weath api and get weather
-// const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Atlanta.JSON?access_token=pk.eyJ1Ijoic3dpZnRtYXBsZSIsImEiOiJjanRjNjZyNGEwaGN1M3lxandlbzRnbGQ5In0.EWCNMHBKSS6VY6o5UFh6Ew&limit=1'
-
-// request({url: geoUrl, json: true}, (error, response) => {
-//     // console.log(response.body.message)
-//     if (error) {
-//         console.log('Unable to connect to geolocation service!')
-//     } else if (response.body.message) {
-//         console.log('Unable to find location!')
-//     } else {
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(`The latitude is ${latitude} the longitude is ${longitude}`)
-//     }
-// })
-
-const geocode = (address, callback) => {
-    const encodedURI = encodeURIComponent(address)
-    const url  = `https://api.mapbox.com/geocoding/v5/mapbox.places/` + address + `.JSON?access_token=pk.eyJ1Ijoic3dpZnRtYXBsZSIsImEiOiJjanRjNjZyNGEwaGN1M3lxandlbzRnbGQ5In0.EWCNMHBKSS6VY6o5UFh6Ew&limit=1`
-
-    request({ url: url, json: true }, (error, response) => {
-        if (error) {
-            callback('Unable to connect to location services!')
-        } else if (response.body.features.length ===  0) {
-            callback('Unable to find location!')
-        } else {
-            callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
-            })
-        }
-    })
-}
-
-geocode('Atlanta', (error, data) => {
+geocode('Boston', (error, data) => {
     console.log('error',  error)
     console.log('Data', data)
+})
+
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+    console.log(`${data.summary} It is currently ${data.temperature} degrees out. There is a ${data.precipProb} % chance of rain`)
 })
