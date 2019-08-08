e710b1e00eb1fd8f761f1ed3fc746db151ed5f0f
Anthony Moss <asmoss93@gmail.com>
2019-08-07 23:06:17 -0400

    updates geocode http request to asyc function

--- app.js
+++ app.js
@@ -16,17 +16,41 @@ const request = require('request');
 
 // Geocoding
 // user provides address => get lat/long from geo api => put lat/long into weath api and get weather
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Atlanta.JSON?access_token=pk.eyJ1Ijoic3dpZnRtYXBsZSIsImEiOiJjanRjNjZyNGEwaGN1M3lxandlbzRnbGQ5In0.EWCNMHBKSS6VY6o5UFh6Ew&limit=1'
+// const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Atlanta.JSON?access_token=pk.eyJ1Ijoic3dpZnRtYXBsZSIsImEiOiJjanRjNjZyNGEwaGN1M3lxandlbzRnbGQ5In0.EWCNMHBKSS6VY6o5UFh6Ew&limit=1'
 
-request({url: geoUrl, json: true}, (error, response) => {
-    // console.log(response.body.message)
-    if (error) {
-        console.log('Unable to connect to geolocation service!')
-    } else if (response.body.message) {
-        console.log('Unable to find location!')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(`The latitude is ${latitude} the longitude is ${longitude}`)
-    }
+// request({url: geoUrl, json: true}, (error, response) => {
+//     // console.log(response.body.message)
+//     if (error) {
+//         console.log('Unable to connect to geolocation service!')
+//     } else if (response.body.message) {
+//         console.log('Unable to find location!')
+//     } else {
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(`The latitude is ${latitude} the longitude is ${longitude}`)
+//     }
+// })
+
+const geocode = (address, callback) => {
+    const encodedURI = encodeURIComponent(address)
+    const url  = `https://api.mapbox.com/geocoding/v5/mapbox.places/` + address + `.JSON?access_token=pk.eyJ1Ijoic3dpZnRtYXBsZSIsImEiOiJjanRjNjZyNGEwaGN1M3lxandlbzRnbGQ5In0.EWCNMHBKSS6VY6o5UFh6Ew&limit=1`
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services!')
+        } else if (response.body.features.length ===  0) {
+            callback('Unable to find location!')
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+geocode('Atlanta', (error, data) => {
+    console.log('error',  error)
+    console.log('Data', data)
 })
