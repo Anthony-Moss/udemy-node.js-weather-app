92b65ace38d55d96fdb1dd0702f3529fd71db06b
Anthony Moss <asmoss93@gmail.com>
2019-08-08 18:56:42 -0400

    destructutres response

--- utils/geocode.js
+++ utils/geocode.js
@@ -4,16 +4,16 @@ const geocode = (address, callback) => {
     const encodedURI = encodeURIComponent(address)
     const url  = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedURI}.JSON?access_token=pk.eyJ1Ijoic3dpZnRtYXBsZSIsImEiOiJjanRjNjZyNGEwaGN1M3lxandlbzRnbGQ5In0.EWCNMHBKSS6VY6o5UFh6Ew&limit=1`
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!')
-        } else if (response.body.features.length ===  0) {
+        } else if (body.features.length ===  0) {
             callback('Unable to find location!')
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
