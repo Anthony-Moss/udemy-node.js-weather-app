0cc44091d3b09bc23319a85c0a5d72f952d476b6
Anthony Moss <asmoss93@gmail.com>
2019-08-08 19:46:54 -0400

    adds examples of how to add html and json sent via server

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -3,20 +3,23 @@ const express = require('express');
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello Express!')
+    res.send('<h1>Weather</h1>')
 })
 
 
 app.get('/help', (req, res) => {
-    res.send('Help Page')
+    res.send('<h1>Help Page</h1>')
 })
 
 app.get('/about', (req, res) => {
-    res.send('Title page')
+    res.send('<h1>Weather App</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast: 'Sunny',
+        location: 'Atlanta'
+    })
 })
 
 app.listen(3001, () => {
